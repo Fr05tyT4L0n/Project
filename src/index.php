4b31f1e01da63b8406b73345e64135ce4fc98659
Fr05tyT4l0n <takdanai.duangoprn@e-tech.ac.th>
2025-11-15 11:15:17 +0700

    Update: Project Structure

--- src/index.php
+++ src/index.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>โปรเจค</title>
     <!-- bootstrap 5 css -->
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
 </head>
 <body>
     
-<!-- bootstrap 5 js -->
- <script src="/assets/js/bootstrap.bundle.min.js"></script>
+    <!-- bootstrap 5 js -->
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
