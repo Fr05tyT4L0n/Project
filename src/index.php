efec90c559ad07b462fc39c2dd8548f147d2a696
Fr05tyT4l0n <takdanai.duangoprn@e-tech.ac.th>
2025-11-15 11:05:20 +0700

    first commit

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>โปรเจค</title>
+    <!-- bootstrap 5 css -->
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+</head>
+<body>
+    
+<!-- bootstrap 5 js -->
+ <script src="/assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
